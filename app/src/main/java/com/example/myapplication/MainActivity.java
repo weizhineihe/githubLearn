3d3b76fa98bbfb49b7ada3744bd0f602d6d74552
liudeai\Administrator <liudeai@shangminet.net>
2022-12-29 09:19:42 +0800

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button showBar = (Button) findViewById(R.id.show_snackbar_bt);
+        showBar.setOnClickListener(v -> {
+            Snackbar.make(findViewById(R.id.my_cl),"你好",Snackbar.LENGTH_LONG).setAction("点击事件",v1 -> {
+                Toast.makeText(this,"我好",Toast.LENGTH_SHORT).show();
+            }).show();
+        });
     }
+
 }
